bd9c0ec74afd3317f4b81549b476dfc1ff65bbae
Esten Leonardsen <estenleonardsen@gmail.com>
2021-10-28 10:13:46 +0200

    More precise setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='pyment',
       version='1.0',
-      description='Deep learning models for neuroimaging',
+      description='Deep learning models for neuroimaging data',
       author='Esten H. Leonardsen',
       author_email='estenhl@psykologi.uio.no',
       url='https://github.com/estenhl/pyment-public',
