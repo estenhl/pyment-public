ca0cb0b7e0ee9f901e88d20013a9eab792a17dff
Esten Leonardsen <estenleonardsen@gmail.com>
2021-11-01 20:51:19 +0100

    Added pretrained weights for soft classification and ranking SFCN

--- pyment/models/utils/weight_repository.py
+++ pyment/models/utils/weight_repository.py
@@ -13,6 +13,26 @@ _mapping = {
         'url': ('https://drive.google.com/u/0/'
                 'uc?id=1Mev57Rdst5TAxGEt3OUY97ToKOWq0uCC&export=download'),
         'filename': 'regression_sfcn_brain_age_weights_no_top.hdf5'
+    },
+    ('SoftClassificationSFCN', 'brain-age', True): {
+        'url': ('https://drive.google.com/u/0/'
+                'uc?id=1_sQu4HoLUIHXcL0qO9sHcsKnH-WCVGDK&export=download'),
+        'filename': 'soft_classification_sfcn_brain_age_weights.hdf5'
+    },
+    ('SoftClassificationSFCN', 'brain-age', False): {
+        'url': ('https://drive.google.com/u/0/'
+                'uc?id=1gO-LW96lQVCMp6mZ_rfbTkrgiHacgIoS&export=download'),
+        'filename': 'soft_classification_sfcn_brain_age_weights_no_top.hdf5'
+    },
+    ('RankingSFCN', 'brain-age', True): {
+        'url': ('https://drive.google.com/u/0/'
+                'uc?id=1cElx0AufQWDAKUWoELFQ4piBbgE4AIAz&export=download'),
+        'filename': 'ranking_sfcn_brain_age_weights.hdf5'
+    },
+    ('RankingSFCN', 'brain-age', False): {
+        'url': ('https://drive.google.com/u/0/'
+                'uc?id=1-2s2psZD2R9-ReTOHHJLzJZeYJAxQGBK&export=download'),
+        'filename': 'ranking_sfcn_brain_age_weights_no_top.hdf5'
     }
 }
 
