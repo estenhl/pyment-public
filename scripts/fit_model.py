9ecb7ce9a8c83d489c4f563be6e8dce2d6d1dde2
Esten Leonardsen <estenleonardsen@gmail.com>
2021-11-25 13:22:33 +0100

    Working on fit model script

--- scripts/fit_model.py
+++ scripts/fit_model.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 
 from functools import reduce
+from tensorflow.distribute import MirroredStrategy
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.optimizers import Adam
 from tensorflow.python.keras.callbacks import LearningRateScheduler
@@ -31,157 +32,191 @@ logging.basicConfig(format=logformat, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def fit_model(*, model: str, model_kwargs: str = '{}', training: List[str],
-              validation: List[str], batch_size: int, num_threads: int,
-              loss: str, metrics: str, learning_rate_schedule: Any = 1e-3,
+              validation: List[str], target: str = None, batch_size: int, 
+              num_threads: int, loss: str, metrics: str,
+              learning_rate_schedule: Any = 1e-3, epochs: int,
               destination: str):
     set_seed(42)
 
-    if os.path.isdir(destination):
-        raise ValueError(f'Folder {destination} already exists')
+    with MirroredStrategy().scope():
+        if os.path.isdir(destination):
+            raise ValueError(f'Folder {destination} already exists')
 
-    model_kwargs = json.loads(model_kwargs)
+        model_kwargs = json.loads(model_kwargs)
 
-    model = get_model(model, **model_kwargs)
+        model = get_model(model, **model_kwargs)
 
-    training = [load_dataset_from_jsonfile(filename) for filename in training]
-    training = reduce(lambda x, y: x + y, training)
-    logger.info((f'Training on dataset with {len(training)} samples with y '
-                 f'ranging from {round(np.amin(training.y), 2)} '
-                 f'to {round(np.amax(training.y), 2)} '
-                 f'(mean {round(np.mean(training.y), 2)})'))
+        training = [load_dataset_from_jsonfile(filename) \
+                    for filename in training]
+        training = reduce(lambda x, y: x + y, training)
 
-    validation = [load_dataset_from_jsonfile(filename) \
-                  for filename in validation]
-    validation = reduce(lambda x, y: x + y, validation)
-    logger.info((f'Validating on dataset with {len(validation)} samples with y '
-                 f'ranging from {round(np.amin(validation.y), 2)} '
-                 f'to {round(np.amax(validation.y), 2)} '
-                 f'(mean {round(np.mean(validation.y), 2)})'))
+        if target is not None:
+            training.target = target
 
-    preprocessor = lambda x: x / 255.
+        training = training[np.where(~np.isnan(training.y))[0]]
+        
+        logger.info((f'Training on dataset with {len(training)} samples with '
+                    f'y ranging from {round(np.amin(training.y), 2)} '
+                    f'to {round(np.amax(training.y), 2)} '
+                    f'(mean {round(np.mean(training.y), 2)})'))
 
-    training_generator = AsyncNiftiGenerator(training, 
-                                             preprocessor=preprocessor,
-                                             batch_size=batch_size,
-                                             threads=num_threads,
-                                             shuffle=True,
-                                             infinite=True)
+        validation = [load_dataset_from_jsonfile(filename) \
+                    for filename in validation]
+        validation = reduce(lambda x, y: x + y, validation)
 
-    validation_generator = AsyncNiftiGenerator(validation,
-                                               preprocessor=preprocessor,
-                                               batch_size=batch_size,
-                                               threads=num_threads,
-                                               shuffle=False,
-                                               infinite=True)
+        if target is not None:
+            validation.target = target
+        
+        validation = validation[np.where(~np.isnan(validation.y))[0]]
+        
+        logger.info((f'Validating on dataset with {len(validation)} samples '
+                    f'with y ranging from {round(np.amin(validation.y), 2)} '
+                    f'to {round(np.amax(validation.y), 2)} '
+                    f'(mean {round(np.mean(validation.y), 2)})'))
 
-    os.mkdir(destination)
-    checkpoints = os.path.join(destination, 'checkpoints')
+        preprocessor = lambda x: x / 255.
 
-    callbacks = [Resetter(training_generator),
-                 ModelCheckpoint(checkpoints, save_best_only=True),
-                 EarlyStopping(patience=5, restore_best_weights=True)]
+        training_generator = AsyncNiftiGenerator(training, 
+                                                preprocessor=preprocessor,
+                                                batch_size=batch_size,
+                                                threads=num_threads,
+                                                shuffle=True,
+                                                infinite=True)
 
-    learning_rate = learning_rate_schedule
+        validation_generator = AsyncNiftiGenerator(validation,
+                                                preprocessor=preprocessor,
+                                                batch_size=batch_size,
+                                                threads=num_threads,
+                                                shuffle=False,
+                                                infinite=True)
 
-    if isinstance(learning_rate, str):
-        try:
-            learning_rate = float(learning_rate)
-        except Exception:
-            if os.path.isfile(learning_rate):
-                schedule = LearningRateSchedule.from_jsonfile(learning_rate)
-                callbacks.append(LearningRateScheduler(schedule))
-                learning_rate = schedule(0)
+        os.mkdir(destination)
+        checkpoints = os.path.join(destination, 'checkpoints')
 
-    model.compile(loss=loss, metrics=metrics, optimizer=Adam(learning_rate))
+        callbacks = [Resetter(training_generator),
+                    ModelCheckpoint(checkpoints, save_best_only=True),
+                    EarlyStopping(patience=5, restore_best_weights=True)]
 
-    history = model.fit(training_generator, 
-                        validation_data=validation_generator,
-                        steps_per_epoch=training_generator.batches,
-                        validation_steps=validation_generator.batches,
-                        epochs=1, callbacks=callbacks)
-    
-    with open(os.path.join(destination, 'history.json'), 'w') as f:
-        json.dump(json_serialize_object(history.history), f)
+        learning_rate = learning_rate_schedule
 
-    model.save(os.path.join(destination, 'model'))
+        if isinstance(learning_rate, str):
+            try:
+                learning_rate = float(learning_rate)
+            except Exception:
+                if os.path.isfile(learning_rate):
+                    schedule = \
+                        LearningRateSchedule.from_jsonfile(learning_rate)
+                    callbacks.append(LearningRateScheduler(schedule))
+                    learning_rate = schedule(0)
 
-    training_generator.reset()
-    training_generator.infinite = False
-    training_ids = training_generator.dataset.ids
+        model.compile(loss=loss, metrics=metrics, 
+                      optimizer=Adam(learning_rate))
 
-    logger.info((f'Predicting for {training_generator.batches} training '
-                 'batches'))
+        history = model.fit(training_generator, 
+                            validation_data=validation_generator,
+                            steps_per_epoch=training_generator.batches,
+                            validation_steps=validation_generator.batches,
+                            epochs=epochs, callbacks=callbacks)
+        
+        with open(os.path.join(destination, 'history.json'), 'w') as f:
+            json.dump(json_serialize_object(history.history), f)
 
-    training_predictions, training_labels = model.predict(training_generator, 
+        model.save(os.path.join(destination, 'model'))
+
+        training_generator.reset()
+        training_generator.infinite = False
+        training_ids = training_generator.dataset.ids
+
+        logger.info((f'Predicting for {training_generator.batches} training '
+                    'batches'))
+
+        training_predictions, training_labels = \
+            model.predict(training_generator, return_labels=True)
+
+        validation_generator.reset()
+        validation_generator.infinite = False
+        validation_ids = validation_generator.dataset.ids
+
+        logger.info((f'Predicting for {validation_generator.batches} '
+                    'validation batches'))
+
+        validation_predictions, \
+            validation_labels = model.predict(validation_generator,
                                             return_labels=True)
 
-    validation_generator.reset()
-    validation_generator.infinite = False
-    validation_ids = validation_generator.dataset.ids
-
-    logger.info((f'Predicting for {validation_generator.batches} validation '
-                 'batches'))
-
-    validation_predictions, \
-        validation_labels = model.predict(validation_generator,
-                                          return_labels=True)
-
-    if isinstance(training.target, Label):
-        label = training.target
-        decoded_training_predictions = label.revert(training_predictions)
-        decoded_training_labels = label.revert(training_labels)
-        decoded_validation_predictions = label.revert(validation_predictions)
-        decoded_validation_labels = label.revert(validation_labels)
-                                                              
-    if model.type == ModelType.REGRESSION:
-        training_predictions = np.squeeze(training_predictions)
-        validation_predictions = np.squeeze(validation_predictions)
-
         if isinstance(training.target, Label):
-            decoded_training_predictions = \
-                np.squeeze(decoded_training_predictions)
+            label = training.target
+            decoded_training_predictions = label.revert(training_predictions)
+            decoded_training_labels = label.revert(training_labels)
             decoded_validation_predictions = \
-                np.squeeze(decoded_validation_predictions)
+                label.revert(validation_predictions)
+            decoded_validation_labels = label.revert(validation_labels)
+                                                                
+        if model.type == ModelType.REGRESSION:
+            training_predictions = np.squeeze(training_predictions)
+            validation_predictions = np.squeeze(validation_predictions)
 
-        training_mae = np.mean(np.abs(training_predictions - training_labels))
-        print(f'Training MAE: {round(training_mae, 4)}')
+            if isinstance(training.target, Label):
+                decoded_training_predictions = \
+                    np.squeeze(decoded_training_predictions)
+                decoded_validation_predictions = \
+                    np.squeeze(decoded_validation_predictions)
 
-        validation_mae = np.mean(np.abs(validation_predictions - \
-                                        validation_labels))
-        print(f'Validation MAE: {round(validation_mae, 4)}')
+            training_mae = np.mean(np.abs(training_predictions - \
+                                          training_labels))
+            print(f'Training MAE: {round(training_mae, 4)}')
 
-        training_df = pd.DataFrame({
-            'id': training_ids,
-            'label': training_labels,
-            'prediction': training_predictions
-        })
+            validation_mae = np.mean(np.abs(validation_predictions - \
+                                            validation_labels))
+            print(f'Validation MAE: {round(validation_mae, 4)}')
 
-        validation_df = pd.DataFrame({
-            'id': validation_ids,
-            'label': validation_labels,
-            'prediction': validation_predictions
-        })
+            results = {
+                'training_mae': training_mae,
+                'validation_mae': validation_mae
+            }
 
-        if isinstance(training.target, Label):
-            decoded_training_mae = np.mean(np.abs(decoded_training_predictions - \
-                                                  decoded_training_labels))
-            print(f'Decoded training MAE: {decoded_training_mae}')
+            training_df = pd.DataFrame({
+                'id': training_ids,
+                'label': training_labels,
+                'prediction': training_predictions
+            })
 
-            decoded_validation_mae = np.mean(np.abs(decoded_validation_predictions - \
-                                                    decoded_validation_labels))
-            print(f'Decoded validation MAE: {decoded_validation_mae}')
+            validation_df = pd.DataFrame({
+                'id': validation_ids,
+                'label': validation_labels,
+                'prediction': validation_predictions
+            })
 
-            training_df['decoded_label'] = decoded_training_labels
-            training_df['decoded_prediction'] = decoded_training_predictions
-            validation_df['decoded_label'] = decoded_validation_labels
-            validation_df['decoded_prediction'] = decoded_validation_predictions
+            if isinstance(training.target, Label):
+                decoded_training_mae = \
+                    np.mean(np.abs(decoded_training_predictions - \
+                                   decoded_training_labels))
+                print(f'Decoded training MAE: {decoded_training_mae}')
 
-        training_df.to_csv(os.path.join(destination, 
-                                        'training_predictions.csv'),
-                           index=False)
-        validation_df.to_csv(os.path.join(destination,
-                                          'validation_predictions.csv'),
-                             index=False)
+                decoded_validation_mae = \
+                    np.mean(np.abs(decoded_validation_predictions - \
+                                   decoded_validation_labels))
+                print(f'Decoded validation MAE: {decoded_validation_mae}')
+
+                results['decoded_training_mae'] = decoded_training_mae
+                results['decoded_validation_mae'] = decoded_validation_mae
+
+                training_df['decoded_label'] = decoded_training_labels
+                training_df['decoded_prediction'] = \
+                    decoded_training_predictions
+                validation_df['decoded_label'] = decoded_validation_labels
+                validation_df['decoded_prediction'] = \
+                    decoded_validation_predictions
+
+            with open(os.path.join(destination, 'results.json'), 'w') as f:
+                json.dump(results, f)
+
+            training_df.to_csv(os.path.join(destination, 
+                                            'training_predictions.csv'),
+                            index=False)
+            validation_df.to_csv(os.path.join(destination,
+                                            'validation_predictions.csv'),
+                                index=False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Fits a model on the given dataset')
@@ -196,6 +231,8 @@ if __name__ == '__main__':
                         help='Path to JSON files containing training data')
     parser.add_argument('-v', '--validation', required=True, nargs='+',
                         help='Path to JSON files containing validation data')
+    parser.add_argument('-x', '--target', required=False, default=None,
+                        help='Target used by the model')
     parser.add_argument('-bs', '--batch_size', required=True, type=int,
                         help=('Batch size used by the generators for training '
                               'and validation'))
@@ -208,14 +245,17 @@ if __name__ == '__main__':
                         help='Metrics logged during model training')
     parser.add_argument('-lr', '--learning_rate_schedule', required=False, 
                         default=1e-3, help='Learning rate used by optimizer')
+    parser.add_argument('-n', '--epochs', required=True, type=int,
+                        help='Number of epochs to run training for')
     parser.add_argument('-d', '--destination', required=True,
                         help='Folder where results are stored')
                             
     args = parser.parse_args()
 
     fit_model(model=args.model, model_kwargs=args.model_kwargs, 
-              training=args.training, validation=args.validation,
-              batch_size=args.batch_size, num_threads=args.num_threads,
-              loss=args.loss, metrics=args.metrics, 
+              training=args.training, validation=args.validation, 
+              target=args.target, batch_size=args.batch_size, 
+              num_threads=args.num_threads, loss=args.loss, 
+              metrics=args.metrics, 
               learning_rate_schedule=args.learning_rate_schedule, 
-              destination=args.destination)
+              epochs=args.epochs, destination=args.destination)
