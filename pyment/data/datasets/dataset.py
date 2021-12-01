aed9a7c6ce19042188fe94eaa7bc0ee30e280664
Esten Leonardsen <estenleonardsen@gmail.com>
2021-12-01 19:24:55 +0100

    Linted some files

--- pyment/data/datasets/dataset.py
+++ pyment/data/datasets/dataset.py
@@ -15,7 +15,7 @@ class Dataset(ABC):
     @abstractproperty
     def y(self) -> np.ndarray:
         """Returns the target vector of the dataset
-        
+
         Returns:
             np.ndarray: The y-vector of the dataset
         """
@@ -24,7 +24,7 @@ class Dataset(ABC):
     @abstractproperty
     def json(self) -> Dict[str, Any]:
         """Returns a json representation of the dataset
-        
+
         Returns:
             dict: A json representation of the dataset
         """
@@ -33,7 +33,7 @@ class Dataset(ABC):
     @property
     def jsonstring(self) -> str:
         """Returns a json string representing the dataset
-        
+
         Returns:
             str: A jsonstring representing the dataset
         """
@@ -41,10 +41,10 @@ class Dataset(ABC):
 
     @classmethod
     def from_json(cls, obj: Dict[str, Any]) -> Dataset:
-        """Instantiates a Dataset of the given class from the provided 
+        """Instantiates a Dataset of the given class from the provided
         object. Properties of the object must match the corresponding
         constructor
-        
+
         Returns:
             Dataset: A dataset instantiated with the keyword parameters
             given to this function.
@@ -55,7 +55,7 @@ class Dataset(ABC):
     def from_jsonstring(cls, jsonstring: str) -> Dataset:
         """Instantiates a Dataset from a jsonstring, via the from_json
         function
-        
+
         Returns:
             Dataset: A dataset instantiated with the keyword parameters
             coded by the string given to this function.
@@ -64,7 +64,7 @@ class Dataset(ABC):
 
     def save(self, path: str) -> bool:
         """Saves a json representation of the object to file
-        
+
         Args:
             path (str): Path where the object is saved
         Returns:
@@ -75,7 +75,7 @@ class Dataset(ABC):
     @abstractproperty
     def __len__(self) -> int:
         """Returns length of the dataset
-        
+
         Returns:
             int: Length of the dataset
         """
@@ -83,7 +83,7 @@ class Dataset(ABC):
 
     def __eq__(self, other: Dataset) -> bool:
         """Check equality between this dataset and another
-        
+
         Returns:
             bool: Whether the two datasets are equal or not
         """
