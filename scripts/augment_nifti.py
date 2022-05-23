3ac13ee1cd99e91c92fa4ae1e90791fe2a56f85b
Esten Leonardsen <estenleonardsen@gmail.com>
2022-05-23 11:34:28 +0200

    Bugfixes in augmentations

--- scripts/augment_nifti.py
+++ scripts/augment_nifti.py
@@ -17,34 +17,47 @@ logformat = '%(asctime)s - %(levelname)s - %(name)s: %(message)s'
 logging.basicConfig(format=logformat, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def augment_nifti(augmenter: str, image: str, iterations: int = 1):
+
+def _pad(image: np.ndarray, size: int) -> np.ndarray:
+    padding = size - np.asarray(image.shape)
+    before = padding // 2
+    after = padding - before
+    padding = list(zip(before, after))
+
+    return np.pad(image, padding)
+
+
+def augment_nifti(augmenter: str, image: str, iterations: int = 1,
+                  destination: str = None):
     np.random.seed(42)
 
     augmenter = NiftiAugmenter.from_file(augmenter)
 
-    print(augmenter)
-
     image = nib.load(image).get_fdata()
-    min_value = np.amin(image)
-    max_value = np.amax(image)
     start = time()
     augmentations = [augmenter(image) for _ in range(iterations)]
     logger.info((f'Performed {iterations} augmentations in {time() - start} '
                  'seconds'))
 
+
+    max_dim = np.amax(image.shape)
+    canvas = np.zeros((max_dim * (iterations + 1), max_dim * 3))
+
+    image = _pad(image, max_dim)
+    augmentations = [_pad(augmented, max_dim) for augmented in augmentations]
     means = np.asarray(image.shape) // 2
 
-    fig, ax = plt.subplots(iterations + 1, len(means), figsize=(15, 10*(iterations + 1)))
     for i in range(len(means)):
-        ax[0][i].imshow(np.take(image, means[i], axis=i), cmap='Greys_r',
-                        clim=(min_value, max_value))
-        ax[0][i].axis('off')
+        slice = np.take(image, means[i], axis=i)
+        canvas[0:max_dim,max_dim*i:max_dim*(i+1)] = slice
 
         for j in range(len(augmentations)):
-            ax[1+j][i].imshow(np.take(augmentations[j], means[i], axis=i),
-                              cmap='Greys_r', clim=(min_value, max_value))
-            ax[1+j][i].axis('off')
+            slice = np.take(augmentations[j], means[i], axis=i)
+            canvas[max_dim*(j+1):max_dim*(j+2),max_dim*i:max_dim*(i+1)] = slice
 
+    plt.figure(figsize=(5, 20))
+    plt.imshow(canvas, cmap='Greys_r')
+    plt.axis('off')
     plt.show()
 
 
