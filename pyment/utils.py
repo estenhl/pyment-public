fd4b4bd614b787492f670bb17f309fb7868b609b
Esten Leonardsen <estenleonardsen@gmail.com>
2024-04-05 22:54:46 +0200

    Added unit tests for the weight repository

--- pyment/utils.py
+++ pyment/utils.py
@@ -1,8 +1,7 @@
-import math
 import os
-import requests
 
-from tqdm import tqdm
+DATA_DIR = os.path.join(os.path.expanduser('~'), '.pyment')
+MODELS_DIR = os.path.join(DATA_DIR, 'models')
 
-ROOT_DIR = os.path.join(os.path.expanduser('~'), '.pyment')
-MODELS_DIR = os.path.join(ROOT_DIR, 'models')
+ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+METADATA_DIR = os.path.join(ROOT_DIR, '.data')
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,27 @@
 import os
 import sys
 
+from shutil import rmtree
+
+
+TESTS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+ROOT_FOLDER = os.path.join(TESTS_FOLDER, os.pardir)
+METADATA_FOLDER = os.path.join(ROOT_FOLDER, '.data')
 
 # Appends the root pyment directory to the path
 def pytest_configure(config):
     testpath = os.path.dirname(os.path.abspath(__file__))
     libpath = os.path.join(testpath, os.pardir)
     sys.path.append(libpath)
+
+class TemporaryFolder:
+    def __init__(self, path: str = os.path.join(TESTS_FOLDER, 'tmp')):
+        self.path = path
+
+    def __enter__(self):
+        os.mkdir(self.path)
+
+        return self.path
+
+    def __exit__(self, *args):
+        rmtree(self.path)
