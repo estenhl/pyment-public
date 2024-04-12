994c4b99618d66a3b3bbfe69a3a598f6db925a80
Esten Leonardsen <estenleonardsen@gmail.com>
2024-04-12 13:10:57 +0200

    Finished READMEs for now

--- pyment/utils.py
+++ pyment/utils.py
@@ -4,4 +4,4 @@ DATA_DIR = os.path.join(os.path.expanduser('~'), '.pyment')
 MODELS_DIR = os.path.join(DATA_DIR, 'models')
 
 ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
-METADATA_DIR = os.path.join(ROOT_DIR, '.data')
+METADATA_DIR = os.path.join(ROOT_DIR, 'data')
