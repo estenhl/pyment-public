b335e48d3ced8ab599a4dcca3ebeb503d7feeccc
Esten Leonardsen <estenleonardsen@gmail.com>
2024-09-01 22:40:04 +0200

    Minor modification to data import

--- pyment/utils.py
+++ pyment/utils.py
@@ -7,6 +7,6 @@ MODELS_DIR = os.path.join(DATA_DIR, 'models')
 ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
 
 try:
-    METADATA_DIR = pkg_resources.files('pyment.data')
+    METADATA_DIR = pkg_resources.files('pyment').joinpath('data')
 except:
     METADATA_DIR = os.path.join(ROOT_DIR, 'data')
