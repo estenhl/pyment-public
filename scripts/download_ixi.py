f03777be0c2c7c33715d129787103db351978102
Esten Leonardsen <estenleonardsen@gmail.com>
2025-12-22 21:25:55 +0100

    Finished CLI and updated README

--- scripts/download_ixi.py
+++ scripts/download_ixi.py
@@ -5,10 +5,46 @@ import requests
 import tarfile
 from tqdm import tqdm
 
-from pyment.utils.download_file import download_file
-
 
 DEFAULT_DESTINATION = os.path.join(os.path.expanduser('~'), 'data', 'ixi')
+
+def download_file(
+    url: str,
+    destination: str,
+    description: str = None,
+    decode_github: bool = False
+) -> str:
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+        total_size = int(response.headers.get('content-length', 0))
+
+        # 1 MB chunks
+        chunk_size = 1<<20
+
+        progress_bar = tqdm(
+            response.iter_content(chunk_size=chunk_size),
+            total=int(math.ceil(total_size / chunk_size)),
+            unit='mb',
+            unit_scale=True,
+            unit_divisor=1024,
+            desc=description
+        )
+        progress_bar.format_dict['rate'] = f'mb/s'
+
+        with open(destination, 'wb') as f:
+            for chunk in progress_bar:
+                f.write(chunk)
+
+    if decode_github:
+        # Assumes a JSON file downloaded from GitHub
+        with open(destination, 'rb') as f:
+            data = json.load(f)
+
+        data = base64.b64decode(data['content'])
+
+        with open(destination, 'wb') as f:
+            f.write(data)
+
 
 def download_tar(tar_path: str) -> str:
     url = (
@@ -44,7 +80,7 @@ def download_ixi(destination: str):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        'Downloads T1 images and demographic data fromthe IXI dataset'
+        'Downloads T1 images and demographic data from the IXI dataset'
     )
     parser.add_argument(
         '-d', '--destination',
